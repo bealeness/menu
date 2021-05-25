88ae1e9b4c0ca0dbbb4c482d7a7b2e1a96c199d4
Beale Stainton <bealeness@gmail.com>
2021-05-25 17:53:01 +1200

    Cors added to settings. Connection made to react frontend

--- core/settings.py
+++ core/settings.py
@@ -46,11 +46,13 @@ INSTALLED_APPS = [
     'menu',
     'menu_api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -141,3 +143,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAllowAny',
     ],
 }
+
+# Cors Allowed Orign URLs
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
