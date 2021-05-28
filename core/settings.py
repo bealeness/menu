605b95e1522e49b9d63e1c819a2f8e9c57193e0d
Beale Stainton <bealeness@gmail.com>
2021-05-28 16:53:00 +1200

    Blacklisted tokens added. Keep testing it

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'users',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -162,7 +163,7 @@ AUTH_USER_MODEL = 'users.NewUser'
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
 
--- users/models.py
+++ users/models.py
@@ -49,7 +49,7 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
    objects = CustomAccountManager()
  
    USERNAME_FIELD = 'email'
-   REQUIRED_FIELDS = ['username', 'first_name']
+   REQUIRED_FIELDS = ['username']
  
    def __str__(self):
        return self.username
